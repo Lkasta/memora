35944a08613d8e41b9a7918ebbd4e124d39df30c
Lkasta <lkastabackup@gmail.com>
2025-08-28 00:39:34 -0300

    style: update logo button in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -1,12 +1,14 @@
 "use client";
 import { MemorieItemSidebar } from "./MemorieItemSidebar";
 import { NewMemorie } from "./NewMemorie";
-import Link from "next/link";
 import { useMemories } from "@/service/memories/memories.hook";
 import { Loader2Icon } from "lucide-react";
+import { Button } from "../ui/button";
+import { useRouter } from "next/navigation";
 
 export function Sidebar() {
   const { data: memories, isLoading } = useMemories();
+  const router = useRouter();
 
   if (isLoading) {
     return (
@@ -21,12 +23,15 @@ export function Sidebar() {
 
   return (
     <div className="flex h-screen w-full max-w-72 flex-col border-r">
-      <Link
-        href="/"
-        className="px-6 py-3 text-2xl font-bold text-gray-800 select-none"
-      >
-        <span className="text-violet-500">me</span>mora
-      </Link>
+      <div className="px-6 py-3 select-none">
+        <Button
+          variant="ghost"
+          onClick={() => router.push("/")}
+          className="h-min w-min cursor-pointer gap-0 p-0 text-2xl font-bold text-gray-800 hover:bg-transparent hover:text-gray-900 !transition-all"
+        >
+          <span className="text-violet-500">me</span>mora
+        </Button>
+      </div>
       <div className="px-6 py-3">
         <NewMemorie />
       </div>
