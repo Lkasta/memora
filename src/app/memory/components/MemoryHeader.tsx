d0d0124b0133781cec8163eecd92e3c36778ca8e
Lkasta <lkastabackup@gmail.com>
2025-08-28 00:32:37 -0300

    feat: implement back memorie button

--- src/app/memory/components/MemoryHeader.tsx
+++ src/app/memory/components/MemoryHeader.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { formatMemorieDateDetailed } from "@/utils/dates";
 import { ChevronLeft, Loader2Icon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 type Props = {
   eventDate: Date;
@@ -8,12 +10,20 @@ type Props = {
 };
 
 export function MemoryHeader({ eventDate, isSaving, title }: Props) {
+  const router = useRouter();
+
   return (
     <div className="grid h-14 flex-shrink-0 grid-cols-3 items-center border-b px-6 py-3">
       <div className="flex items-center gap-2 text-gray-700">
-        <ChevronLeft size={16} />
-        <span className="text-sm font-bold">{title || "Sem título"}</span>
-        {isSaving && <Loader2Icon className="animate-spin" size={12} />}
+        <Button
+          variant="ghost"
+          onClick={router.back}
+          className="flex cursor-pointer items-center gap-2 !transition-all"
+        >
+          <ChevronLeft size={16} />
+          <span className="text-sm font-bold">{title || "Sem título"}</span>
+          {isSaving && <Loader2Icon className="animate-spin" size={12} />}
+        </Button>
       </div>
       <h1 className="text-center text-sm font-bold text-gray-500">
         {formatMemorieDateDetailed(eventDate)}
