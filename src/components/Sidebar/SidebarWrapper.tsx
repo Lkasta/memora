d658afc22153fae2954db6e6dcb1305723d09de0
Lkasta <lkastabackup@gmail.com>
2025-09-01 22:26:24 -0300

    feat: no render sidebar on publicroutes

--- src/components/Sidebar/SidebarWrapper.tsx
+++ src/components/Sidebar/SidebarWrapper.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Sidebar } from "@/components/Sidebar/Sidebar";
+import { publicRoutes } from "@/utils/routes";
 import { usePathname } from "next/navigation";
 
 export function SidebarWrapper() {
-  const route = usePathname();
-  if (route === "/login") return null;
+  const pathname = usePathname();
+
+  if (publicRoutes.includes(pathname)) return null;
   return <Sidebar />;
 }
