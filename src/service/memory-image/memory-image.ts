85a6b2bb25ebad36209472db0684cc7e14f2968f
Lkasta <lkastabackup@gmail.com>
2025-09-17 15:53:29 -0300

    feat(images-routes): added update and delete images functions

--- src/service/memory-image/memory-image.ts
+++ src/service/memory-image/memory-image.ts
@@ -6,3 +6,15 @@ export async function uploadMemorieImage(payload: FormData) {
   const { data } = await api.post("/images/", payload);
   return data;
 }
+
+export async function updateMemorieImage(payload: FormData) {
+  const { data } = await api.put("/images/", payload);
+  return data;
+}
+
+export async function deleteMemorieImage(payload: FormData) {
+  const { data } = await api.delete("/images/", {
+    data: payload,
+  });
+  return data;
+}
--- src/service/memory-image/memory-images.hook.ts
+++ src/service/memory-image/memory-images.hook.ts
@@ -2,7 +2,11 @@
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 
-import { uploadMemorieImage } from "./memory-image";
+import {
+  deleteMemorieImage,
+  updateMemorieImage,
+  uploadMemorieImage,
+} from "./memory-image";
 
 export function useUploadImage() {
   const queryClient = useQueryClient();
@@ -11,6 +15,39 @@ export function useUploadImage() {
     mutationFn: uploadMemorieImage,
     onSuccess: (_, variables) => {
       queryClient.invalidateQueries({ queryKey: ["memories"] });
+      if (variables.get("memorie_id")) {
+        queryClient.invalidateQueries({
+          queryKey: ["memories", variables.get("memorie_id")],
+        });
+      }
+    },
+  });
+}
+
+export function useUpdateImage() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: updateMemorieImage,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["memories"] });
+      if (variables.get("memorie_id")) {
+        queryClient.invalidateQueries({
+          queryKey: ["memories", variables.get("memorie_id")],
+        });
+      }
+    },
+  });
+}
+
+export function useDeleteImage() {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: deleteMemorieImage,
+    onSuccess: (_, variables) => {
+      queryClient.invalidateQueries({ queryKey: ["memories"] });
+
       if (variables.get("memorie_id")) {
         queryClient.invalidateQueries({
           queryKey: ["memories", variables.get("memorie_id")],
